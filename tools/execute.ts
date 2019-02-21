62cff500b7e5b398bc46c1ebbd7fca84c90bf826
Isaac Williams <isaac.williams@lifeomic.com>
2019-02-21 14:39:53 -0500

    Pull in boilerplate from managed-integration-example

---
+++ tools/execute.ts
@@ -0,0 +1,30 @@
+/* tslint:disable:no-console */
+import {
+  createLocalInvocationEvent,
+  executeSingleHandlerLocal,
+} from "@jupiterone/jupiter-managed-integration-sdk/local";
+import { createLogger, TRACE } from "bunyan";
+import { executionHandler } from "../src/index";
+
+async function run(): Promise<void> {
+  const logger = createLogger({ name: "local", level: TRACE });
+
+  const integrationConfig = {
+    // providerApiToken: process.env.PROVIDER_LOCAL_EXECUTION_API_TOKEN
+  };
+
+  logger.info(
+    await executeSingleHandlerLocal(
+      integrationConfig,
+      logger,
+      executionHandler,
+      createLocalInvocationEvent(),
+    ),
+    "Execution completed successfully!",
+  );
+}
+
+run().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
